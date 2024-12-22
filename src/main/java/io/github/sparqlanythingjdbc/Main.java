05ebe91054607f4de396058283640f20b3e39ffe
Joffrey Bienvenu <joffrey.bienvenu@infrabel.be>
2024-12-22 18:29:02 +0100

    feat: Add sparqlanything as dependency

--- src/main/java/io/github/sparqlanythingjdbc/Main.java
+++ src/main/java/io/github/sparqlanythingjdbc/Main.java
@@ -1,8 +1,16 @@
 package io.github.sparqlanythingjdbc;
 import java.sql.*;
+import io.github.sparqlanything.engine.FacadeX;
+import org.apache.jena.query.*;
+import org.apache.jena.sparql.engine.main.QC;
 
 public class Main {
     public static void main(String[] args) {
+        // mainDriver();
+        mainSparqlAnything();
+    }
+
+    public static void mainDriver() {
         try {
             Class.forName("io.github.sparqlanythingjdbc.Driver");
 
@@ -23,5 +31,26 @@ public class Main {
         } catch (ClassNotFoundException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    public static void mainSparqlAnything(){
+
+        // Set FacadeX OpExecutor as default executor factory
+        QC.setFactory(ARQ.getContext(), FacadeX.ExecutorFactory);
+
+        // Execute the query by using standard Jena ARQ's API
+        Dataset kb = DatasetFactory.createGeneral();
+
+        Query query = QueryFactory.create(
+                "PREFIX fx:  <http://sparql.xyz/facade-x/ns/> " +
+                        "PREFIX xyz: <http://sparql.xyz/facade-x/data/> " +
+                        "SELECT ?o { " +
+                        "SERVICE <x-sparql-anything:> { " +
+                        "fx:properties fx:content '[1,2,3]' ; " +
+                        "fx:media-type 'application/json' . " +
+                        "?s fx:anySlot ?o" +
+                        "}}");
+
+        System.out.println(ResultSetFormatter.asText(QueryExecutionFactory.create(query,kb).execSelect()));
     }
 }
