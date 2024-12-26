d24603ddcc82bb4b503bb09f3bf2ce827ab4317f
Joffrey Bienvenu <joffrey.bienvenu@infrabel.be>
2024-12-26 17:41:03 +0100

    fix: use immutable maps for JDBCTypeMap

--- src/main/java/io/github/sparqlanythingjdbc/utils/JDBCTypeMap.java
+++ src/main/java/io/github/sparqlanythingjdbc/utils/JDBCTypeMap.java
@@ -4,59 +4,54 @@ import java.math.BigDecimal;
 import java.math.BigInteger;
 import java.net.URI;
 import java.sql.Types;
-import java.util.HashMap;
 import java.util.Map;
 
 public class JDBCTypeMap {
-    private static final Map<Class<?>, Integer> javaClassMap = new HashMap<>();
-    private static final Map<String, Integer> uriMap = new HashMap<>();
-    private static final Map<Integer, String> labelMap = new HashMap<>();
+
+    private static final Map<Class<?>, Integer> javaClassMap = Map.ofEntries(
+            Map.entry(Float.class, Types.FLOAT),
+            Map.entry(Double.class, Types.DOUBLE),
+            Map.entry(Integer.class, Types.INTEGER),
+            Map.entry(Long.class, Types.BIGINT),
+            Map.entry(Short.class, Types.SMALLINT),
+            Map.entry(Byte.class, Types.BINARY),
+            Map.entry(BigDecimal.class, Types.DECIMAL),
+            Map.entry(BigInteger.class, Types.BIGINT),
+            Map.entry(Boolean.class, Types.BOOLEAN),
+            Map.entry(String.class, Types.VARCHAR),
+            Map.entry(URI.class, Types.VARCHAR)
+    );
+
+    private static final Map<String, Integer> uriMap = Map.of(
+            "http://www.w3.org/2001/XMLSchema#date", Types.DATE,
+            "http://www.w3.org/2001/XMLSchema#dateTime", Types.TIMESTAMP,
+            "http://www.w3.org/2001/XMLSchema#time", Types.TIME
+    );
+
+    private static final Map<Integer, String> labelMap = Map.ofEntries(
+            Map.entry(Types.ARRAY, "ARRAY"),
+            Map.entry(Types.BIGINT, "BIGINT"),
+            Map.entry(Types.BINARY, "BINARY"),
+            Map.entry(Types.BIT, "BIT"),
+            Map.entry(Types.BLOB, "BLOB"),
+            Map.entry(Types.BOOLEAN, "BOOLEAN"),
+            Map.entry(Types.CHAR, "CHAR"),
+            Map.entry(Types.CLOB, "CLOB"),
+            Map.entry(Types.DATE, "DATE"),
+            Map.entry(Types.DECIMAL, "DECIMAL"),
+            Map.entry(Types.DOUBLE, "DOUBLE"),
+            Map.entry(Types.FLOAT, "FLOAT"),
+            Map.entry(Types.INTEGER, "INTEGER"),
+            Map.entry(Types.NVARCHAR, "NVARCHAR"),
+            Map.entry(Types.REAL, "REAL"),
+            Map.entry(Types.SMALLINT, "SMALLINT"),
+            Map.entry(Types.TIME, "TIME"),
+            Map.entry(Types.TIMESTAMP, "TIMESTAMP"),
+            Map.entry(Types.VARCHAR, "VARCHAR"),
+            Map.entry(Types.NULL, "NULL")
+    );
+
     private static final Integer defaultType = Types.NULL;
-
-    // XSD types defined with JavaClass in org.apache.jena.datatypes.xsd.XSDDatatype
-    static {
-        javaClassMap.put(Float.class, Types.FLOAT);
-        javaClassMap.put(Double.class, Types.DOUBLE);
-        javaClassMap.put(Integer.class, Types.INTEGER);
-        javaClassMap.put(Long.class, Types.BIGINT);
-        javaClassMap.put(Short.class, Types.SMALLINT);
-        javaClassMap.put(Byte.class, Types.BINARY);
-        javaClassMap.put(BigDecimal.class, Types.DECIMAL);
-        javaClassMap.put(BigInteger.class, Types.BIGINT);
-        javaClassMap.put(Boolean.class, Types.BOOLEAN);
-        javaClassMap.put(String.class, Types.VARCHAR);
-        javaClassMap.put(URI.class, Types.VARCHAR);
-    }
-
-    // XSD types defined without JavaClass, and only with XSD uri in org.apache.jena.datatypes.xsd.XSDDatatype
-    static {
-        uriMap.put("http://www.w3.org/2001/XMLSchema#date", Types.DATE);
-        uriMap.put("http://www.w3.org/2001/XMLSchema#dateTime", Types.TIMESTAMP);
-        uriMap.put("http://www.w3.org/2001/XMLSchema#time", Types.TIME);
-    }
-
-    static {
-        labelMap.put(Types.ARRAY, "ARRAY");
-        labelMap.put(Types.BIGINT, "BIGINT");
-        labelMap.put(Types.BINARY, "BINARY");
-        labelMap.put(Types.BIT, "BIT");
-        labelMap.put(Types.BLOB, "BLOB");
-        labelMap.put(Types.BOOLEAN, "BOOLEAN");
-        labelMap.put(Types.CHAR, "CHAR");
-        labelMap.put(Types.CLOB, "CLOB");
-        labelMap.put(Types.DATE, "DATE");
-        labelMap.put(Types.DECIMAL, "DECIMAL");
-        labelMap.put(Types.DOUBLE, "DOUBLE");
-        labelMap.put(Types.FLOAT, "FLOAT");
-        labelMap.put(Types.INTEGER, "INTEGER");
-        labelMap.put(Types.NVARCHAR, "NVARCHAR");
-        labelMap.put(Types.REAL, "REAL");
-        labelMap.put(Types.SMALLINT, "SMALLINT");
-        labelMap.put(Types.TIME, "TIME");
-        labelMap.put(Types.TIMESTAMP, "TIMESTAMP");
-        labelMap.put(Types.VARCHAR, "VARCHAR");
-        labelMap.put(Types.NULL, "NULL");
-    }
 
     public static int getTypeInt(Class<?> javaClass) {
         return javaClassMap.getOrDefault(javaClass, defaultType);
